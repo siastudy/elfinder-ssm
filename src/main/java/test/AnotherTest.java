068796708e18113ec206deb6e43c66c7e8640cb9
vincent <siastudy@163.com>
2018-04-24 17:57:10 +0800

    Logger added.

--- src/main/java/test/AnotherTest.java
+++ src/main/java/test/AnotherTest.java
@@ -1,12 +1,20 @@
 package test;
 
+
+import org.apache.log4j.Logger;
+
 public class AnotherTest {
     private static AnotherTest ourInstance = new AnotherTest();
+    private static Logger logger = Logger.getLogger(AnotherTest.class);
 
     public static AnotherTest getInstance() {
         return ourInstance;
     }
 
     private AnotherTest() {
+
+        System.out.print("this is a test");
+        logger.info("hello");
+
     }
 }
